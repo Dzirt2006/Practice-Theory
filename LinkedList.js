3edaf7deaa4283a5f1c795b9b6f63326c4648bb9
Dzirt2006 <dzirt-2006@mail.ru>
2020-08-25 20:25:09 -0400

    ll merge, pramp q

--- LinkedList.js
+++ LinkedList.js
@@ -1,54 +1,53 @@
-
 //make new list
 
 function ListNode(val, next) {
-    this.val = (val===undefined ? undefined : val)
-    this.next = (next===undefined ? null : next)
+    this.val = (val === undefined ? undefined : val)
+    this.next = (next === undefined ? null : next)
 }
 
-const filing=(list,val)=>{
+const filing = (list, val) => {
     let tail;
-    if(list.val){
-        tail=list.next;
-        if(tail.val) {
+    if (list.val) {
+        tail = list.next;
+        if (tail.val) {
             while (tail.next) {
                 tail = tail.next;
             }
             tail.next = new ListNode(val);
-        }else{
-            tail.val=val;
+        } else {
+            tail.val = val;
         }
-    }else{
-        list.val=val;
-        list.next=new ListNode();
+    } else {
+        list.val = val;
+        list.next = new ListNode();
     }
     return list;
 }
 
-function running(list){
-    let tail=list;
-    const arr=[];
-    while(tail){
+function running(list) {
+    let tail = list;
+    const arr = [];
+    while (tail) {
 
         arr.push(tail.val);
-        tail=tail.next;
+        tail = tail.next;
     }
     return arr;
 }
 
 
-const list=new ListNode();
-const list2=new ListNode();
+const list = new ListNode();
+const list2 = new ListNode();
 
-const la2=[2,6,7,8]
-const la1=[1,2,3,4,5,9,10];
-la1.forEach(x=>{
-    filing(list,x)
+const la2 = [2, 6, 7, 8]
+const la1 = [1, 2, 3, 4, 5, 9, 10];
+la1.forEach(x => {
+    filing(list, x)
 })
 
 
-la2.forEach(x=>{
-    filing(list2,x)
+la2.forEach(x => {
+    filing(list2, x)
 })
 // console.log(list)
 // console.log(list2)
@@ -94,15 +93,15 @@ la2.forEach(x=>{
 
 
 function reverseLinkedList(head) {
-    let pointer=head.next;
-    head.next=null;
-    let node= head;
-    while(pointer){
+    let pointer = head.next;
+    head.next = null;
+    let node = head;
+    while (pointer) {
         console.log(head)
-        head=pointer;
-        pointer=head.next;
-        head.next=node;
-        node=head;
+        head = pointer;
+        pointer = head.next;
+        head.next = node;
+        node = head;
     }
     // console.log(running(list))
     return head;
@@ -120,36 +119,41 @@ function reverseLinkedList(head) {
 // [1,2,3,4,5,9,10];
 
 function mergeLinkedLists(headOne, headTwo) {
-    let list=headOne.val>headTwo.val?headTwo:headOne;
-    let tail=list;
-    let t1=headOne.val<headTwo.val?headTwo:headOne;
-    console.log(list,t1)
-    while(t1 && tail){
+    let list = headOne.val > headTwo.val ? headTwo : headOne;
+    let tail = list;
+    let t1 = headOne.val < headTwo.val ? headTwo : headOne;
+    console.log(list, t1)
+    while (t1 && tail) {
 // console.log(t1,tail)
-        if(t1.val<tail.val){
-            const temp=new ListNode(tail.val,tail.next);
-            tail.val=t1.val;
-            tail.next=temp;
-            t1=t1.next;
+        if (t1.val < tail.val) {
+            const temp = new ListNode(tail.val, tail.next);
+            tail.val = t1.val;
+            tail.next = temp;
+            t1 = t1.next;
             // console.log(tail.next)
-            if(tail.next){
-                tail=tail.next;
-            }else{break;}
-        }else{
-            if(tail.next){
-                tail=tail.next;
-            }else{break;}
+            if (tail.next) {
+                tail = tail.next;
+            } else {
+                break;
+            }
+        } else {
+            if (tail.next) {
+                tail = tail.next;
+            } else {
+                break;
+            }
         }
     }
 
-    while(t1){
-        tail.next=new ListNode(t1.val)
-        tail=tail.next;
-        t1=t1.next;
+    while (t1) {
+        tail.next = new ListNode(t1.val)
+        tail = tail.next;
+        t1 = t1.next;
     }
     console.log(list)
 
 }
+
 //
 // mergeLinkedLists(list,list2)
 // console.log(running(list));
@@ -157,35 +161,35 @@ function mergeLinkedLists(headOne, headTwo) {
 //------------------------------------------------------------------------------------------------------------------
 
 //CREATES NEW LIST!
-var mergeTwoLists = function(l1, l2) {
-    let list=new ListNode();
-    let tail1=l1;
-    let tail2=l2;
-    let tailR=list;
-    while(tail1 || tail2){
+var mergeTwoLists = function (l1, l2) {
+    let list = new ListNode();
+    let tail1 = l1;
+    let tail2 = l2;
+    let tailR = list;
+    while (tail1 || tail2) {
         // console.log(!!tail1,!!tail2)
         let val1;
         let val2;
-        if(tail1.val>tail2.val){
-            val1= tail2.val;
-            val2=tail1.val;
-        }else{
-            val1=tail1.val;
-            val2=tail2.val;
+        if (tail1.val > tail2.val) {
+            val1 = tail2.val;
+            val2 = tail1.val;
+        } else {
+            val1 = tail1.val;
+            val2 = tail2.val;
         }
-        if(tailR.val){
-            tailR.next=new ListNode(val1);
-            tailR=tailR.next;
-            tailR.next=new ListNode(val2);
-            tailR=tailR.next;
-        }else{
-            tailR.val=val1;
-            tailR.next=new ListNode(val2);
-            tailR=tailR.next;
+        if (tailR.val) {
+            tailR.next = new ListNode(val1);
+            tailR = tailR.next;
+            tailR.next = new ListNode(val2);
+            tailR = tailR.next;
+        } else {
+            tailR.val = val1;
+            tailR.next = new ListNode(val2);
+            tailR = tailR.next;
         }
         // console.log(tailR)
-        tail1=tail1.next;
-        tail2=tail2.next;
+        tail1 = tail1.next;
+        tail2 = tail2.next;
         // console.log('list!!!',list);
     }
 
@@ -194,6 +198,30 @@ var mergeTwoLists = function(l1, l2) {
 
 //
 // console.log('res',running(mergeTwoLists(list,list2)));
+
+function mergeLinkedListsTwo(headOne, headTwo) {
+    let head1=headOne;
+    let head2=headTwo;
+    let previous;
+    while(head2!==null && head1!==null){
+        if(head1.val<head2.val){
+            previous=head1;
+            head1=head1.next;
+        }else{
+            if(previous) {
+                previous.next=head2;
+            }
+                previous=head2;
+                head2=head2.next;
+                previous.next=head1
+
+        }
+    }
+    if(!head1) previous.next=head2;
+    return headOne.val<headTwo.val? headOne:headTwo;
+}
+
+console.log('res', running(mergeLinkedListsTwo(list, list2)));
 
 //----------------------------------------------------------------------------------------------------------------------------
 
@@ -214,8 +242,8 @@ function removeKthNodeFromEnd(head, n) {
         }
     }
     if (!tail) {
-        head.val=head.next.val;
-        head.next=head.next.next;
+        head.val = head.next.val;
+        head.next = head.next.next;
     } else {
         tail.next = f.next;
     }
@@ -230,43 +258,42 @@ function removeKthNodeFromEnd(head, n) {
 //----------------------------------------------------------------------------------------------------------------------------
 
 
-let algoArr=[1,2,3,4,5,6];
-let list3={}
-algoArr.forEach(x=>{
-    filing(list3,x)
+let algoArr = [1, 2, 3, 4, 5, 6];
+let list3 = {}
+algoArr.forEach(x => {
+    filing(list3, x)
 })
 
 
-
 function shiftLinkedList(head, n) {
-    const length=calcLength(head,n);
-    let moveIndx=0;
-    if(n===length)return head
-    console.log(moveIndx,length)
-    if(n>0){
-        moveIndx=n%length===n ? length-n:length-n%length;
-    }else if(n<0){
-        moveIndx=Math.abs(n)%length===n ? n*-1 : Math.abs(n)%length;
+    const length = calcLength(head, n);
+    let moveIndx = 0;
+    if (n === length) return head
+    console.log(moveIndx, length)
+    if (n > 0) {
+        moveIndx = n % length === n ? length - n : length - n % length;
+    } else if (n < 0) {
+        moveIndx = Math.abs(n) % length === n ? n * -1 : Math.abs(n) % length;
     }
 
-    if(moveIndx===0 || moveIndx===length) return head
-    for(let i=1;i<=moveIndx;i++){
-        if(i===moveIndx){
-            let tail=head;
-            head=head.next;
-            tail.next=null;
-        }else{
-            head=head.next;
+    if (moveIndx === 0 || moveIndx === length) return head
+    for (let i = 1; i <= moveIndx; i++) {
+        if (i === moveIndx) {
+            let tail = head;
+            head = head.next;
+            tail.next = null;
+        } else {
+            head = head.next;
         }
     }
     return head
 }
 
-const calcLength=(list,n)=>{
-    if(n===0)return 0;
-    n=Math.abs(n)
-    let length=0;
-    let tail=list;
+const calcLength = (list, n) => {
+    if (n === 0) return 0;
+    n = Math.abs(n)
+    let length = 0;
+    let tail = list;
     while (tail) {
         length++;
         console.log(!tail.next && length !== n && n % length !== 0)
@@ -281,4 +308,4 @@ const calcLength=(list,n)=>{
 }
 
 
-console.log(running(shiftLinkedList(list3,18)));
+// console.log(running(shiftLinkedList(list3, 18)));
--- Searching.js
+++ Searching.js
@@ -1,13 +1,11 @@
-
-
 function binarySearch(array, target) {
     const middle = (array.length / 2) | 0;
-    if (array[0]===target || array[middle]===target) {
+    if (array[0] === target || array[middle] === target) {
         return true
     }
     if (array[middle] > target) {
         const arr = array.slice(0, middle)
-        let res= binarySearch(arr, target);
+        let res = binarySearch(arr, target);
     } else if (array[middle] < target) {
         const arr = array.slice(middle)
         binarySearch(arr, target);
@@ -20,3 +18,27 @@ function binarySearch(array, target) {
 // console.log(binarySearch([0, 1, 21, 32, 33, 34, 45, 45, 61, 71, 72, 73], 33));
 
 //----------------------------------------------------------------------------------------------------------------------------
+/*
+input:  shiftArr = [9, 12, 17, 2, 4, 5], num = 2 # shiftArr is the
+                                                 # outcome of shifting
+                                                 # [2, 4, 5, 9, 12, 17]
+                                                 # three times to the left
+
+output: 3 # since it’s the index of 2 in arr
+ */
+function shiftedArrSearch(shiftArr, num) {
+    let lastIndex = shiftArr.length - 1;
+    let count = 0;
+    while (shiftArr[0] !== num && count < lastIndex) {
+        let shift=shiftArr.shift();
+        shiftArr.push(shift);
+        count++;
+    }
+    if(shiftArr[0]===num){
+        return count;
+    }else{
+        return -1;
+    }
+}
+
+console.log(shiftedArrSearch([9, 12, 17, 26, 4, 5], num = 2));
