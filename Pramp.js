dca3225dd9ea8dac9714ffe1735d6449ab40c1cc
Dzirt2006 <dzirt-2006@mail.ru>
2020-09-11 13:12:58 -0400

    array shifted search workable version

---
+++ Pramp.js
@@ -0,0 +1,95 @@
+// My question
+/**
+ * A sorted array of distinct integers shiftArr is shifted to the left by an unknown offset
+ * and you don’t have a pre-shifted copy of it.
+ * For instance, the sequence 1, 2, 3, 4, 5 becomes 3, 4, 5, 1, 2, after shifting it twice to the left.
+ * Given shiftArr and an integer num, implement a function shiftedArrSearch that finds and returns the index of num in shiftArr.
+ * If num isn’t in shiftArr, return -1. Assume that the offset can be any value between 0 and arr.length - 1.
+ */
+
+function shiftedArrSearch(shiftArr, num) {
+    let pivot = spliter(shiftArr);
+    if (num <= shiftArr[pivot] && num >= shiftArr[0]) {
+        return binarySearch(shiftArr, 0, pivot, num);
+    } else {
+        return binarySearch(shiftArr, pivot + 1, shiftArr.length - 1, num);
+    }
+
+}
+
+function binarySearch(arr, start, end, target) {
+    if (end === start) {
+        return arr[start] === target ? start : -1;
+    } else {
+        let middle = (start + end) / 2 | 0;
+        if (target > arr[middle]) {
+            return binarySearch(arr, middle+1, end, target);
+        } else if (target < arr[middle]) {
+            return binarySearch(arr, start, middle-1, target);
+        } else {
+            return middle;
+        }
+    }
+}
+
+//search the pivot point
+function spliter(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (arr[left] > arr[right]) {
+        right--;
+    }
+    return right;
+}
+
+
+console.log("answer ", shiftedArrSearch([9, 12, 17, 2, 4, 5], 4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Searching.js
+++ Searching.js
@@ -16,58 +16,5 @@ function binarySearch(array, target, startIndex = 0, endIndex = array.length - 1
 // console.log(binarySearch([0, 1, 21, 32, 33, 34, 45, 45, 61, 71, 72, 73], 73));
 
 //----------------------------------------------------------------------------------------------------------------------------
-/*
-input:  shiftArr = [9, 12, 17, 2, 4, 5], num = 2 # shiftArr is the
-                                                 # outcome of shifting
-                                                 # [2, 4, 5, 9, 12, 17]
-                                                 # three times to the left
-*/
-function shiftedArrSearch(shiftArr, num) {
-    const pivot = pivotR(shiftArr, 0, shiftArr.length - 1);
-    if (shiftArr[0] <= num) {
-        return bsearch(shiftArr, 0, pivot, num);
-    } else if (shiftArr[pivot + 1] <= num) {
-        return bsearch(shiftArr, pivot + 1, shiftArr.length - 1, num);
-    }
-}
 
-const bsearch = (arr, start, end, target) => {
-    if (start === end) {
-        return arr[start] === num ? start : -1;
-    } else if (start + 1 === end) {
-        if (arr[start] === target) {
-            return start;
-        }
-        if (arr[end] === target) {
-            return end;
-        }
-        return -1;
-    } else {
-        let midle = start + Math.floor((end - start) / 2);
-        if (arr[midle] > target) {
-            end = midle;
-            return bsearch(arr, start, end, target);
-        } else if (arr[midle] < target) {
-            start = midle;
-            return bsearch(arr, start, end, target);
-        } else {
-            return midle;
-        }
-
-    }
-}
-
-function pivotR(arr, start, end) {
-    while (arr[start] > arr[end]) {
-        end--;
-    }
-    return end;
-}
-
-// console.log(shiftedArrSearch([9, 12, 17, 2, 4, 5], num = 9));
-// console.log(shiftedArrSearch([9, 12, 17, 2, 4, 5], num = 12));
-// console.log(shiftedArrSearch([9, 12, 17, 2, 4, 5], num = 17));
-// console.log(shiftedArrSearch([9, 12, 17, 2, 4, 5], num = 2));
-// console.log(shiftedArrSearch([9, 12, 17, 2, 4, 5], num = 4));
-// console.log(shiftedArrSearch([1, 2, 3, 4, 5, 0], num = 0));
 
