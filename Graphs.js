0b9561652496a0888b6c7eab563510a6bd801ebd
Dzirt2006 <dzirt-2006@mail.ru>
2020-09-14 17:31:42 -0400

    breadth first graph search

--- Graphs.js
+++ Graphs.js
@@ -11,13 +11,25 @@ class Node {
 
     depthFirstSearch(array) {
         array.push(this.name);
-        for(let i=0;i<this.children.length;i++){
+        for (let i = 0; i < this.children.length; i++) {
             this.children[i].depthFirstSearch(array);
+        }
+        return array;
+    }
+
+    breadthFirstSearch(array) {
+        const queue = [this];
+        while (queue.length > 0) {
+            let current = queue.shift();
+            array.push(current.name);
+            for (const children of current.children) {
+                queue.push(children);
+            }
         }
         return array;
     }
 
 }
 
-let graph=new Node('A');
+let graph = new Node('A');
 
